c13a4208d22a0c65413f98a0a8ab8517c477a9fd
Michael Petersen <michael_petersen_email@yahoo.com>
2020-06-12 10:54:22 -0700

    recognize input and convert to integer if able.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 // Business Logic
 const numConverter = function(num) {
   let array = [];
-  let numInput = "";
+  // let numberInput = "";
   while (num > 0) {
     if (num > 0)
     num -= 1;
     array.push(num);
+    console.log(num)
   }
 }
 
@@ -14,16 +15,22 @@ const numConverter = function(num) {
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault(event);
-    const numberInput = parseInt($("input[name=number]").val());
-    console.log("input accepted and converted to int");
-    if (!numInput || numInput < 0)
-      alert("Please enter a valid positive integer for Mr. Rogers to translate.");
-      return;
+    const numberInput = parseInt($(numInput).val());
+    console.log("You typed " + numberInput);
+    numberInput.forEach(function(number) {
+
+    })
+    // if (!numInput || numInput < 0)
+    //   alert("Please enter a valid positive integer for Mr. Rogers to translate.");
+    //   return;
     console.log("alert works");
     const output = numConverter(numInput)
 
     $(".results").show();
+    console.log("results are showing");
     $("#numInput").text(numberInput);
-    $("#output").text(output)
+    console.log("results taken from numInput");
+    $("#output").text(output);
+    console.log("results are being sent to output");
   })
 })
