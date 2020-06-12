70dff1a0f5342afc4420533be79e279f610ce1a6
Michael Petersen <michael_petersen_email@yahoo.com>
2020-06-12 09:29:50 -0700

    begin creating business and UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
 // Business Logic
 const numConverter = function(num) {
+  let array = [];
   let numInput = "";
   while (num > 0) {
-    if (num === )
+    if (num > 0)
+    num -= 1;
+    array.push(num);
   }
 }
 
 // User Interface Logic
 
 $(document).ready(function() {
+  $("form#input").submit(function(event) {
+    event.preventDefault(event);
+    const numberInput = parseInt($("input[name=number]").val());
 
+    if (!numInput || numInput < 0)
+      alert("Please enter a valid positive integer for Mr. Rogers to translate.");
+      return;
+
+    const output = numConverter(numInput)
+
+    $(".results").show();
+    $("#numInput").text(numberInput);
+    $("#output").text(output)
+  })
 })
