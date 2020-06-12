4ab07ef515937425a3c90a2b79b8b64a5f4f5698
Michael Petersen <michael_petersen_email@yahoo.com>
2020-06-12 14:16:44 -0700

    create simple response line and continue modifying js to return appropriate array.

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
     event.preventDefault(event);
     const numberInput = parseInt($(numInput).val());
     console.log("You typed " + numberInput);
-    let inputArray = [0];
-    let returnArray = [];
-    for (let index = 0; index <= numberInput; index += 1) {
-      // let returnArray = returnArray + index;
-      returnArray.push(numInput[index] + 1);
-      $(returnArray).toString(numInput[index]);
-    };
-    console.log(returnArray);
+    // let startingArray = [0, ""];
+    // let returnArray = [];
+    // for (let index = 0; index <= numberInput; index += 1) {
+    //   returnArray.push(numInput[index] + 1);
+    // };
+    // console.log(returnArray);
+    $("#output").text(numberInput);
   })
+  
 })
