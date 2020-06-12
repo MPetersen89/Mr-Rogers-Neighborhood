587f9ff1ef17a32af4d38484c8860fc2724aa354
Michael Petersen <michael_petersen_email@yahoo.com>
2020-06-12 08:26:20 -0700

    started business/UI logic, almost forgot about specs list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+// Business Logic
+const numConverter = function(num) {
+  let numInput = "";
+  while (num > 0) {
+    if (num === )
+  }
+}
+
+// User Interface Logic
+
 $(document).ready(function() {
-  
+
 })
