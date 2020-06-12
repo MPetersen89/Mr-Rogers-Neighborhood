9ec5210c1fa32ca75582cf6d2f4f2f2b5a6bf044
Michael Petersen <michael_petersen_email@yahoo.com>
2020-06-12 15:19:50 -0700

    continue making changes to js to search for a way to make an array work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
 // Business Logic
+const array = parseInt($("input#numInput").val());
+let newArray = [];
+  array.forEach(function(element) {
+    newArray.push(element += 1);
+  })
+  console.log(newArray);
 
-
-// User Interface Logic
-
+  // User Interface Logic
 $(document).ready(function() {
   $("form#input").submit(function(event) {
-    event.preventDefault(event);
+    event.preventDefault();
     const numberInput = parseInt($(numInput).val());
     console.log("You typed " + numberInput);
-    // let startingArray = [0, ""];
-    // let returnArray = [];
-    // for (let index = 0; index <= numberInput; index += 1) {
-    //   returnArray.push(numInput[index] + 1);
-    // };
+    let startingArray = [0];
+    let returnArray = [];
+    for (let index = 0; index <= numberInput; index += 1) {
+      returnArray.push(numInput[index] + 1);
+    };
     // console.log(returnArray);
-    $("#output").text(numberInput);
+    $("#output").text(returnArray);
   })
-  
 })
+
+
+
+
+
