2b22d3694246a2621ba988cf027f212b40a83de7
Michael Petersen <michael_petersen_email@yahoo.com>
2020-06-12 12:34:17 -0700

    continue to modify JS to try to create a working array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,6 @@
 // Business Logic
 
 
-//   if (numInput > 0) {
-//     numInput -= 1;
-//     inputArray.push(numInput);
-//   }
-// return inputArray;
-// console.log(inputArray);
-
-// inputArray.forEach(function(number) {
-//   alert(number); 
-// })
-
-
-
-
-
 // User Interface Logic
 
 $(document).ready(function() {
@@ -25,23 +10,11 @@ $(document).ready(function() {
     console.log("You typed " + numberInput);
     let inputArray = [0];
     let returnArray = [];
-    for (let index = 0; index === numberInput; index += 1) {
-      returnArray.push(inputArray[index] + 1);
+    for (let index = 0; index <= numberInput; index += 1) {
+      // let returnArray = returnArray + index;
+      returnArray.push(numInput[index] + 1);
+      $(returnArray).toString(numInput[index]);
     };
-    returnArray;
     console.log(returnArray);
-    })
-    
-    // if (!numInput || numInput < 0)
-    //   alert("Please enter a valid positive integer for Mr. Rogers to translate.");
-    //   return;
-    // console.log("alert works");
-    // const output = numConverter(numInput)
-
-    // $(".results").show();
-    // console.log("results are showing");
-    // $("#numInput").text(numberInput);
-    // console.log("results taken from numInput");
-    // $("#output").text(output);
-    // console.log("results are being sent to output");
-  });
+  })
+})
