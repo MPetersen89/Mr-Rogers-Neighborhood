7a1f2187309f54b9bc6aa2e88943bc506b85760b
Michael Petersen <michael_petersen_email@yahoo.com>
2020-06-24 20:20:23 -0700

    introduce newArray variable, toString() method, and else statement to complete functioning project, utilizing String.prototype.includes() method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,19 @@
 // Business Logic
-let newArray = [];
-  
-function beepBoop(array) {
-  for(let i=0; i <= array.length; i++) {
-    // let arrayFilter = newArray.filter((number) => {
-    //   return newArray.index[i] >= 10
-    // }) 
-    // console.log(arrayFilter);
-    if (array[i] >= 10) {
-      array.split(", ");
-      newArray.push(array);
-    };
-    if (array[i].contains("3")) {
-      newArray.text("Won't you be my neighbor?")
-      newArray.push(array[i]);
-      console.log(newArray);
-    } else if (array[i].contains("2")) {
-      newArray.text("Boop!")
-      newArray.push(array[i]);
-      console.log(newArray);
-    } else if (array[i].contains("1")) {
-      newArray.text("Beep!")
-      newArray.push(array[i]);
-      console.log(newArray);
-    };
+function beepBoop(numberInput) {
+  let newArray = [];
+  for(let i=0; i <= numberInput; i++) {
+    let number = i.toString();
+    if (number.includes(3)) {
+      newArray.push("Won't you be my neighbor?");
+    } else if (number.includes(2)) {
+      newArray.push("Boop!");
+    } else if (number.includes(1)) {
+      newArray.push("Beep!");
+    } else {
+      newArray.push(number);
+    }
   } return newArray;
 };
-
-
-  // for (let i = 0; index <= array; i++)
-  // split outputs >10 and search for 1/2/3, or maybe use contains(), or includes()?
-  
 
   // User Interface Logic
 $(document).ready(function() {
@@ -38,12 +21,7 @@ $(document).ready(function() {
     event.preventDefault();
     const numberInput = parseInt($("#numInput").val());
     let newArray = beepBoop(numberInput);
-    let returnArray = [];
-    for (let index = 0; index <= numberInput; index += 1) {
-      returnArray.push(index);
-    };
     console.log(newArray);
-    console.log(returnArray);
-    $("#output").text(returnArray);
+    $("#output").text(newArray);
   });
 })
